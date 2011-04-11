9671988f21be1b31fb2bf31bd458c9d391867c7e
Nagi Letaifa <nagi.letaifa@zenika.com>
2011-04-11 20:40:12 +0200

    Kicking hello world out from hello-socketio.js

--- hello-socketio.js
+++ hello-socketio.js
@@ -5,16 +5,12 @@ var http = require('http'),
 var SERVER_PORT = 8124;
 
 // Creating HTTP Server
-var server = http.createServer(function(req, res){
-    // Called each time a request is made.
-    res.writeHead(200, {'Content-Type': 'text/plain'});
-    res.end('Hello World\n');
-});
+var server = http.createServer();
 
 // Starting the server
 server.listen(SERVER_PORT);
 
-console.log('Server running on port : ' + SERVER_PORT);
-
 // Attaching Socket.IO to the HTTP Server
 var socket = io.listen(server);
+
+console.log('Server running on port : ' + SERVER_PORT);
